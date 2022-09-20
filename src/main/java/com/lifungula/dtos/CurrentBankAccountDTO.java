3976b351cb310a11d8c2770e4cd74a6a52cb22d1
RollyLif <relifungula@gmail.com>
2022-09-20 09:13:03 +0200

    features: adding a DTOs for saving account and current account

---
+++ src/main/java/com/lifungula/dtos/CurrentBankAccountDTO.java
@@ -0,0 +1,22 @@
+package com.lifungula.dtos;
+
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import com.lifungula.enums.AccountStatus;
+
+import lombok.Data;
+
+@Data 
+public abstract class CurrentBankAccountDTO {
+	@Id
+	private String id;
+	private double balance;
+	private Date createdAt;
+	private AccountStatus status;
+	private CustomerDTO customerDTO;
+	private double overDraft;
+	
+
+}
