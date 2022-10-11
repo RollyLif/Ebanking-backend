ff707b507350efcf10bb54614c19fe5085b83e26
RollyLif <relifungula@gmail.com>
2022-10-11 16:06:12 +0200

    features: adding endpoints for credit, debit and transfer and also DTO for transfer

--- src/main/java/com/lifungula/web/BankAccountRestAPI.java
+++ src/main/java/com/lifungula/web/BankAccountRestAPI.java
@@ -5,14 +5,21 @@ import java.util.List;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lifungula.dtos.AccountHistoryDTO;
 import com.lifungula.dtos.AccountOperationDTO;
 import com.lifungula.dtos.BankAccountDTO;
+import com.lifungula.dtos.CreditDTO;
+import com.lifungula.dtos.DebitDTO;
+import com.lifungula.dtos.TransferRequestDTO;
+import com.lifungula.exception.BalanceNotSufficientException;
 import com.lifungula.exception.BankAccountNotFoundException;
 import com.lifungula.services.BankAccountService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @CrossOrigin("*")
@@ -46,4 +53,21 @@ public class BankAccountRestAPI {
 			@RequestParam(name="size", defaultValue="5") int size) throws BankAccountNotFoundException{
 		return bankAccountService.getAccountHistory(accountId, page, size);
 	}
+	
+	@PostMapping("/accounts/debit")
+	public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+	    this.bankAccountService.debit(debitDTO.getAccountId(), debitDTO.getAmount(), debitDTO.getDescription());
+	    return debitDTO;
+	}
+	
+	@PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(creditDTO.getAccountId(), creditDTO.getAmount(), creditDTO.getDescription());
+        return creditDTO;
+    }
+	
+	@PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.transfer(transferRequestDTO.getAccountSource(), transferRequestDTO.getAccountDestination(), transferRequestDTO.getAmount());
+    }
 }
